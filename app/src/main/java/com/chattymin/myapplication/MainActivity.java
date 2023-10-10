d4f763f55b9f6b966f7e21290717fc075b69dc71
Dongmin <pdm001125@naver.com>
2023-10-10 11:43:27 +0900

    10.10 수업내용 정리
    - 사칙연산 계산기

--- app/src/main/java/com/chattymin/myapplication/MainActivity.java
+++ app/src/main/java/com/chattymin/myapplication/MainActivity.java
@@ -2,16 +2,113 @@ package com.chattymin.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.chattymin.myapplication.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private InputMethodManager imm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        init();
+
+        calc();
+
+    }
+
+    private void init() {
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    }
+
+    private void calc() {
+        binding.buttonPlus.setOnClickListener(view -> {
+            String plusNum = String.valueOf(Integer.parseInt(binding.editTextFirstNum.getText().toString()) + Integer.parseInt(binding.editTextSecondNum.getText().toString()));
+            setText(plusNum);
+            keyboardHide();
+            focuseClear();
+        });
+        binding.buttonMinus.setOnClickListener(view -> {
+            String minusNum = String.valueOf(Integer.parseInt(binding.editTextFirstNum.getText().toString()) - Integer.parseInt(binding.editTextSecondNum.getText().toString()));
+            setText(minusNum);
+            keyboardHide();
+            focuseClear();
+        });
+        binding.buttonMultiply.setOnClickListener(view -> {
+            String multiplyNum = String.valueOf(Integer.parseInt(binding.editTextFirstNum.getText().toString()) * Integer.parseInt(binding.editTextSecondNum.getText().toString()));
+            setText(multiplyNum);
+            keyboardHide();
+            focuseClear();
+        });
+        binding.buttonDivide.setOnClickListener(view -> {
+            String divideNum;
+
+            try {
+                divideNum = String.valueOf(Integer.parseInt(binding.editTextFirstNum.getText().toString()) / Integer.parseInt(binding.editTextSecondNum.getText().toString()));
+            }catch (Exception e){
+                divideNum = "-";
+                makeToast("0으로 나눌 수 없습니다.");
+            }
+
+            setText(divideNum);
+            keyboardHide();
+            focuseClear();
+        });
+    }
+
+    private void setText(String text) {
+        binding.textViewResult.setText("Result : " + text);
+    }
+
+    private void keyboardHide() {
+        if (imm != null)
+            imm.hideSoftInputFromWindow(binding.getRoot().getWindowToken(), 0);
+    }
+
+    private void focuseClear() {
+        if (getCurrentFocus() != null)
+            getCurrentFocus().clearFocus();
+    }
+
+    private void makeToast(String text){
+        Toast.makeText(
+                this,
+                text,
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    /*
+    private void getID(){
+        binding.buttonID.setOnClickListener(view -> {
+            EditText editTextID = binding.editTextID;
+            String id = editTextID.getText().toString();
+
+            keyboardHide();
+            focuseClear();
+
+            binding.textViewID.setText(id);
+            makeToast(id);
+        });
+    }
+
+     */
+
+    /*
 //    class ClickListener implements View.OnClickListener{
 //        @Override
 //        public void onClick(View view) {
@@ -61,4 +158,6 @@ public class MainActivity extends AppCompatActivity {
         textView.setTextColor(Color.rgb(255, 0, 0));
         textView.setSingleLine();
     }
+
+     */
 }
